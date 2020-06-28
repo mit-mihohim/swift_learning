fee81f4db20023f66f615cef87289c82f2f61842
mit-mihohim <mihohim.jp@gmail.com>
2020-06-28 13:05:25 +0900

    my playground swift

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+var age = 32
+
+var population = 8_000_000
+age = 34
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+var awesome = true
+
+var score = 85
+var str3 = "your score was \(score)"
+
+var results = "The test results are here: \(score)"
+
+let taylor = "swift"
+
+let str4 = "Hello, Playground"
+
+let album: String = "Reputation"
+let yesr: Int = 1987
+let height: Double = 1.78
+let taylorRocks: Bool = true

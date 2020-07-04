cbf8ec4cd52b78a7e7bdb16283f7f46ef02882ca
mit-mihohim <mihohim.jp@gmail.com>
2020-07-04 14:07:13 +0900

    day7-3

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -11,41 +11,12 @@ travel {(place: String) in
     print("I'm going to \(place) in my car")
 }
 
-//reminding
-var learnWithUnwrap = {
-    print("Hey, this is fun!")
+func travel1(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
 }
-func learnSwift(using approach: () -> Void) {
-    print("I'm learning Swift")
-    approach()
-}
-learnSwift(using: learnWithUnwrap)
-
-func tendGarden(activities: () -> Void) {
-    print("I love gardening")
-    activities()
-}
-tendGarden {
-    print("Let's grow some roses!")
-}
-
-func knitSweater(then: () -> Void) {
-    print("Buy wool")
-    for _ in 1...100 {
-        print("Knit knit knit...")
-    }
-    then()
-}
-knitSweater {
-    print("Who wants to buy a sweater?")
-}
-
-func goCamping(then action: () -> Void) {
-    print("We're going camping!")
-    action()
-}
-goCamping {
-    print("Sing songs")
-    print("Put up tent")
-    print("Attempt to sleep")
+travel1 {(place: String) -> String in
+    return "I'm going to \(place) in my car"
 }

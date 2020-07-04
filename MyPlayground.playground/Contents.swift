985d92c3a124a1b94833905b27caf2d32996c383
mit-mihohim <mihohim.jp@gmail.com>
2020-07-04 14:44:20 +0900

    day7-5

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,42 +1,56 @@
 import UIKit
 
 //day7 closures
+//
+//func travel(action: (String) -> Void) {
+//    print("I'm getting ready to go.")
+//    action("Ponpi")
+//    print("I arrived!")
+//}
+//travel {(place: String) in
+//    print("I'm going to \(place) in my car")
+//}
+//
+//func travel1(action: (String) -> String) {
+//    print("I'm getting ready to go.")
+//    let description = action("London")
+//    print(description)
+//    print("I arrived!")
+//}
+//travel1 {
+//    "I'm going to \($0) in my car"
+//}
+//
+//func travel2(action: (String, Int) -> String) {
+//    print("I'm getting ready to go.")
+//    let description = action("London", 60)
+//    print(description)
+//    print("I arrived!")
+//}
+//travel2 {
+//    "I'm going to \($0) at \($1) miles per hour."
+//}
+//
+//func travel3() -> (String) -> Void {
+//    return {
+//        print("I'm going to \($0)")
+//    }
+//}
+//let result = travel3()
+//result("London")
+//
+//let result2 = travel3()("Portsmouth")
 
-func travel(action: (String) -> Void) {
-    print("I'm getting ready to go.")
-    action("Ponpi")
-    print("I arrived!")
-}
-travel {(place: String) in
-    print("I'm going to \(place) in my car")
-}
-
-func travel1(action: (String) -> String) {
-    print("I'm getting ready to go.")
-    let description = action("London")
-    print(description)
-    print("I arrived!")
-}
-travel1 {
-    "I'm going to \($0) in my car"
-}
-
-func travel2(action: (String, Int) -> String) {
-    print("I'm getting ready to go.")
-    let description = action("London", 60)
-    print(description)
-    print("I arrived!")
-}
-travel2 {
-    "I'm going to \($0) at \($1) miles per hour."
-}
-
-func travel3() -> (String) -> Void {
+func travel() -> (String) -> Void {
+    var counter = 1
+    
     return {
         print("I'm going to \($0)")
+        print(counter)
+        counter += 1
     }
 }
-let result = travel3()
-result("London")
-
-let result2 = travel3()("Portsmouth")
+let result = travel()
+result("Chichester")
+result("Chichester")
+result("Chichester")

cfb92df3e44341de4b842327a8f1e43edb5e4dc6
mit-mihohim <mihohim.jp@gmail.com>
2020-07-04 13:53:37 +0900

    day7-2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -29,4 +29,23 @@ tendGarden {
     print("Let's grow some roses!")
 }
 
+func knitSweater(then: () -> Void) {
+    print("Buy wool")
+    for _ in 1...100 {
+        print("Knit knit knit...")
+    }
+    then()
+}
+knitSweater {
+    print("Who wants to buy a sweater?")
+}
 
+func goCamping(then action: () -> Void) {
+    print("We're going camping!")
+    action()
+}
+goCamping {
+    print("Sing songs")
+    print("Put up tent")
+    print("Attempt to sleep")
+}

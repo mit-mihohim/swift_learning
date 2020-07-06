511e1ddb57f0a5d5f864b91d4964f59ebe2430e6
mit-mihohim <mihohim.jp@gmail.com>
2020-07-06 12:28:27 +0900

    day9
    - class and struct
    - struct == const
    - class initialized by varible or const
    - class inheritation
    - overriding

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,146 +1,80 @@
 import UIKit
 
-//day8
-
-//struct Sport {
+//day10
+ //class //final for disallow inheritation
+//final class Dog {
 //    var name: String
+//    var breed: String
+//
+//    init(name: String, breed: String) {
+//        self.name = name
+//        self.breed = breed
+//        print("\(name) is a \(breed)")
+//    }
 //}
-//
-//var tennis = Sport(name: "Tennis")
-//print(tennis.name)
-//tennis.name = "Lawn tennis"
-//print(tennis.name)
+//let poppy = Dog(name: "Poppy", breed: "Poodle")
+ //classの継承
+//class Poodle: Dog {
+//    init(name: String) {
+//        super.init(name: name, breed: "Poodle")
+//    }
+//}
+//let moffy = Poodle(name: "Moffy")
 
-//struct Sport {
-//    var name: String
-//    var isOlympicSport: Bool
-//
-//    var olympicStatus: String {
-//        if isOlympicSport {
-//            return "\(name) is an Olympic sport"
-//        } else {
-//            return "\(name) is not an Olympic sport"
-//        }
+
+//override
+//class Dog {
+//    func makeNoise() {
+//        print("Woof!")
 //    }
 //}
 //
-//let chessBoxing = Sport(name: "Chessbocing", isOlympicSport: false)
-//print(chessBoxing.olympicStatus)
-//
-//
-//struct Progress {
-//    var task: String
-//    var amount: Int {
-//        didSet {
-//            print("\(task) is now \(amount)% complete")
-//        }
+//class Poodle: Dog {
+//    override func makeNoise() {
+//        print("Poof!")
 //    }
 //}
-//var progress = Progress(task: "loading data", amount: 0)
-//progress.amount = 30
-//progress.amount = 90
-//progress.amount = 100
-
-//struct City {
-//    var population: Int
-//
-//    func collectTaxes() -> Int {
-//        return population * 1000
-//    }
+//let poppy = Poodle()
+//poppy.makeNoise()
+//class and struct
+//class Singer {
+//    var name = "Metalica"
 //}
+//var singer = Singer()
+//print(singer.name)
 //
-//let london = City(population: 9_000_000)
-//print("population is \(london.population) so we can collect £\(london.collectTaxes())")
-//
-//struct Person {
-//    var name: String
-//
-//    mutating func makeAnonymous() {
-//        name = "Anoymous"
-//    }
-//}
-//var person = Person(name: "Ed")
-//person.makeAnonymous()
-//print(person.name)
+//var singerCopy = singer
+//singerCopy.name = "Lamb Of God"
+//print(singer.name)
 
-//let string = "Do or do not, there is no try."
-//print(string.count)
-//print(string.hasPrefix("Do"))
-//print(string.uppercased())
-//print(string.sorted())
-
-//var toys = ["Woody"]
-//print(toys.count)
-//toys.append("Buzz")
-//toys.firstIndex(of: "Buzz")
-//print(toys.sorted())
-//toys.remove(at: 0)
-//print(toys)
-
-//struct User {
-//    var username: String
+//deinitializer
+//class Person {
+//    var name = "Vin Diesel"
 //
 //    init() {
-//        username = "Anonymous"
-//        print("Creating a new user!")
+//        print("\(name) is arrived!!")
+//    }
+//
+//    func printGreeting() {
+//        print("Fantastic to meet you, Mr.\(name)!!")
+//    }
+//    deinit {
+//        print("\(name) is gone!")
 //    }
 //}
+////var person = Person()
+////person.printGreeting()
 //
-//var user = User()
-//user.username = "towstraws"
-//print(user.username)
+//for _ in 1...3 {
+//    let person = Person()
+//    person.printGreeting()
+//}
 
-//struct Person {
-//    var name: String
-//    init(name: String) {
-//        self.name = name
-//    }
-//}
-//var person = Person(name: "Tom")
-//print(person.name)
+//mutability
+class Singer {
+    var name = "Korn"
+}
 
-//struct FamilyTree {
-//    init() {
-//        print("Creating family tree!")
-//    }
-//}
-//
-//struct Person {
-//    var name: String
-//    lazy var familyTree = FamilyTree()
-//
-//    init(name: String) {
-//        self.name = name
-//    }
-//}
-//var ed = Person(name: "Ed")
-//ed.familyTree
-
-//struct Student {
-//    static var classSize = 0
-//    var name: String
-//
-//    init(name: String) {
-//        self.name = name
-//        Student.classSize += 1
-//    }
-//}
-//
-//let ed = Student(name: "Ed")
-//let taylor = Student(name: "Taylor")
-//print(Student.classSize)
-
-//struct Person {
-//    private var id: String
-//    
-//    init(id: String) {
-//        self.id = id
-//    }
-//    func identify() -> String {
-//        return "My socail security number is \(id)"
-//    }
-//}
-//
-//let ed = Person(id: "12345")
-////print(ed.id)
-//print(ed.identify())
+let korn = Singer()
+korn.name = "Joenasan"
+print(korn.name)
--- MyPlayground2.playground/Contents.swift
+++ MyPlayground2.playground/Contents.swift
@@ -653,3 +653,165 @@ var number = 1
 //result("Chichester")
 //result("Chichester")
 //result("Chichester")
+
+//day8&9section
+
+//struct Sport {
+//    var name: String
+//}
+//
+//var tennis = Sport(name: "Tennis")
+//print(tennis.name)
+//tennis.name = "Lawn tennis"
+//print(tennis.name)
+
+//struct Sport {
+//    var name: String
+//    var isOlympicSport: Bool
+//
+//    var olympicStatus: String {
+//        if isOlympicSport {
+//            return "\(name) is an Olympic sport"
+//        } else {
+//            return "\(name) is not an Olympic sport"
+//        }
+//    }
+//}
+//
+//let chessBoxing = Sport(name: "Chessbocing", isOlympicSport: false)
+//print(chessBoxing.olympicStatus)
+//
+//
+//struct Progress {
+//    var task: String
+//    var amount: Int {
+//        didSet {
+//            print("\(task) is now \(amount)% complete")
+//        }
+//    }
+//}
+//var progress = Progress(task: "loading data", amount: 0)
+//progress.amount = 30
+//progress.amount = 90
+//progress.amount = 100
+
+//struct City {
+//    var population: Int
+//
+//    func collectTaxes() -> Int {
+//        return population * 1000
+//    }
+//}
+//
+//let london = City(population: 9_000_000)
+//print("population is \(london.population) so we can collect £\(london.collectTaxes())")
+//
+//struct Person {
+//    var name: String
+//
+//    mutating func makeAnonymous() {
+//        name = "Anoymous"
+//    }
+//}
+//var person = Person(name: "Ed")
+//person.makeAnonymous()
+//print(person.name)
+
+//let string = "Do or do not, there is no try."
+//print(string.count)
+//print(string.hasPrefix("Do"))
+//print(string.uppercased())
+//print(string.sorted())
+
+//var toys = ["Woody"]
+//print(toys.count)
+//toys.append("Buzz")
+//toys.firstIndex(of: "Buzz")
+//print(toys.sorted())
+//toys.remove(at: 0)
+//print(toys)
+
+//struct User {
+//    var username: String
+//
+//    init() {
+//        username = "Anonymous"
+//        print("Creating a new user!")
+//    }
+//}
+//
+//var user = User()
+//user.username = "towstraws"
+//print(user.username)
+
+//struct Person {
+//    var name: String
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//var person = Person(name: "Tom")
+//print(person.name)
+
+//struct FamilyTree {
+//    init() {
+//        print("Creating family tree!")
+//    }
+//}
+//
+//struct Person {
+//    var name: String
+//    lazy var familyTree = FamilyTree()
+//
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//var ed = Person(name: "Ed")
+//ed.familyTree
+
+//struct Student {
+//    static var classSize = 0
+//    var name: String
+//
+//    init(name: String) {
+//        self.name = name
+//        Student.classSize += 1
+//    }
+//}
+//
+//let ed = Student(name: "Ed")
+//let taylor = Student(name: "Taylor")
+//print(Student.classSize)
+
+//struct Person {
+//    private var id: String
+//
+//    init(id: String) {
+//        self.id = id
+//    }
+//    func identify() -> String {
+//        return "My socail security number is \(id)"
+//    }
+//}
+//
+//let ed = Person(id: "12345")
+////print(ed.id)
+//print(ed.identify())
+
+
+//struct Book {
+//    var totalPages: Int
+//    var pagesLeftToRead = 0
+//    mutating func read(pages: Int) {
+//        if pages < pagesLeftToRead {
+//            pagesLeftToRead -= pages
+//            print("Keep going!")
+//        } else {
+//            pagesLeftToRead = 0
+//            print("I'm done!")
+//        }
+//    }
+//}
+//var swiftStory = Book(totalPages: 100, pagesLeftToRead: 100)
+//swiftStory.read(pages: 30)
